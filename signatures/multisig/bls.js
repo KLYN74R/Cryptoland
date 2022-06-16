a09aa4b35d0b786e131136cabece484213dfe02d
Vlad Chernenko <vladchernenko@protonmail.com>
2022-06-17 00:24:53 +0300

    Fix

--- signatures/multisig/bls.js
+++ signatures/multisig/bls.js
@@ -53,7 +53,7 @@ export default {
    * @param {Number} n - total number of signers
    * 
    */
-    verify_M_N_signature:(pubKeysIn,pubKeysOut,originalGroupPub,data,aggregatedSignaIn,m,n)=>{
+    verify_M_N_signature:async(pubKeysIn,pubKeysOut,originalGroupPub,data,aggregatedSignaIn,m,n)=>{
 
         //0.Get aggregated key of pubkeys who has signed this data
         let signersIn=bls.aggregatePublicKeys(pubKeysIn),
@@ -65,11 +65,11 @@ export default {
             verifiedSignature=await bls.verify(aggregatedSignaIn,data,signersIn)
 
 
-            return verifiedSignature && generalPubKey === originalGroupPub
-            && 
-            (!m || pubKeysIn.length+pubKeysOut.length === n && pubKeysIn.length === m)//if m and n are undefined-we don't interest in number of participants
+        return verifiedSignature && generalPubKey === originalGroupPub
+                                 && 
+                                 (!m || pubKeysIn.length+pubKeysOut.length === n && pubKeysIn.length === m)//if m and n are undefined-we don't interest in number of participants
     
-    },
+    }
 
 
 }
